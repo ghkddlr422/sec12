b1200aae6b8905841741c753af82b037a6ac1f3a
ghkddlr422 <66246456+ghkddlr422@users.noreply.github.com>
2020-06-18 17:15:02 +0900

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+package sec12.exam01_package_compile;
+
+public class A {
+//필드
+	A a1 = new A(true);
+	A a2 = new A(1);
+	A a3 = new A("문자열");
+	
+	//생성자
+	public A(boolean b) {}
+	A(int b) {}
+	private A(String s) {}
+}
